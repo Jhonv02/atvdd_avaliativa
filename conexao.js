4c67b50e4b0e38dadcc5137f4a03f2b8233ccb59
Jhonv02 <jhonvicttordl@gmail.com>
2024-06-14 21:21:01 -0300

    Update conexao.js

--- conexao.js
+++ conexao.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://qhjjnpvtvmlqdhfgukzm.supabase.co'; 
-const supabaseKey = ''; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoampucHZ0dm1scWRoZmd1a3ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0MDA4MzMsImV4cCI6MjAzMzk3NjgzM30.AxiApHRy1jBx66O6RdrBLyD9UG0cmPc4Qg7YbTrj5_U'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
